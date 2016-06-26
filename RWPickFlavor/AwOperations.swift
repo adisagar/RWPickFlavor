73d7c60096dc20c92a4d5877c5cb109e15f8f505
Aditya Prasad <adityaprasad@vmware.com>
2016-06-26 22:31:31 +0530

    Initial Commit

---
+++ RWPickFlavor/AwOperations.swift
@@ -0,0 +1,17 @@
+//
+//  AwOperations.swift
+//  RWPickFlavor
+//
+//  Created by Aditya Prasad on 26/06/16.
+//  Copyright © 2016 Aditya Prasad. All rights reserved.
+//
+
+import UIKit
+
+public class AwOperations: NSObject {
+    
+    public func getName() -> String {
+        return "Airwatch"
+    }
+
+}
